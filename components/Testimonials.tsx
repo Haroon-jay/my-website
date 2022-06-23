802c24f00c7929147b73c5316360e0f1d749f521
Haroon Jawad <haroonjawad6@gmail.com>
2022-06-23 17:20:09 +0500

    Added Testimonials

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -34,17 +34,7 @@ export default function Testimonials() {
         ))}
       </>
 
-      <div>
-        <img src="/images/shirley.png" />
-        <div className="myCarousel">
-          <h3>Shirley Fultz</h3>
-          <h4>Designer</h4>
-          <p>
-            It's freeing to be able to catch up on customized news and not be
-            distracted by a social media element on the same site
-          </p>
-        </div>
-      </div>
+      <div />
     </Carousel>
   );
 }
